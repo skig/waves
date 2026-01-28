dbf88c9e9ff8a89db51b3e563a8e594c17746e6b
ivan iushkov <iushkov.iv@gmail.com>
2026-01-28 23:41:53 +0100

    adding cs_step.py implementation with types for CS step according to values described in BLE Core Spec 6.0, LE CS Subevent Result event chapter

---
+++ toolset/cs_step.py
@@ -0,0 +1,75 @@
+from enum import Enum, IntEnum
+from dataclasses import dataclass
+from typing import Optional
+
+class CSMode(IntEnum):
+    MODE_0 = 0x00
+    MODE_1 = 0x01
+    MODE_2 = 0x02
+    MODE_3 = 0x03
+
+
+class PacketQuality(IntEnum):
+    AA_SUCCESS = 0x00
+    AA_BIT_ERRORS = 0x01
+    AA_NOT_FOUND = 0x02
+
+class PacketNADM(IntEnum):
+    ATTACK_IS_EXTREMELY_UNLIKELY = 0x00
+    ATTACK_IS_VERY_UNLIKELY = 0x01
+    ATTACK_IS_UNLIKELY = 0x02
+    ATTACK_IS_POSSIBLE = 0x03
+    ATTACK_IS_LIKELY = 0x04
+    ATTACK_IS_VERY_LIKELY = 0x05
+    ATTACK_IS_EXTREMELY_LIKELY = 0x06
+    UNKNOWN_NADM = 0xFF
+
+class ToneQualityIndicator(IntEnum):
+    TONE_QUALITY_HIGH = 0x00
+    TONE_QUALITY_MEDIUM = 0x01
+    TONE_QUALITY_LOW = 0x02
+    TONE_QUALITY_UNAVAILABLE = 0x03
+
+class ToneQualityIndicatorExtensionSlot(IntEnum):
+    NOT_TONE_EXTENSION_SLOT = 0x00
+    TONE_EXTENSION_NOT_EXPECTED = 0x01
+    TONE_EXTENSION_EXPECTED = 0x02
+
+
+@dataclass
+class ToneData:
+    pct_i: int # in signed int 12-bit format
+    pct_q: int # in signed int 12-bit format
+    quality: ToneQualityIndicator
+
+
+@dataclass
+class CSStep:
+    mode: CSMode
+    channel: int
+
+
+@dataclass
+class CSStepMode0(CSStep):
+    mode: CSMode = CSMode.MODE_0
+    packet_quality: PacketQuality
+    packet_rssi: int # in dBm
+    packet_antenna: int
+    measured_freq_offset: Optional[float] = None # in 0.01 ppm. Only available on Initiator
+
+
+@dataclass
+class CSStepMode1(CSStep):
+    mode: CSMode = CSMode.MODE_1
+    # TODO: add mode-1 specific fields
+
+@dataclass
+class CSStepMode2(CSStep):
+    mode: CSMode = CSMode.MODE_2
+    antenna_permutation_index: int
+    tones: list[ToneData]
+
+@dataclass
+class CSStepMode3(CSStep):
+    mode: CSMode = CSMode.MODE_3
+    # TODO: add mode-1 specific fields
