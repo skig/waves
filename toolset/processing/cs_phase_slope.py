e7483b97e0fbf3d33d4343ce50c5b3b72ac0b937
ivan iushkov <iushkov.iv@gmail.com>
2026-02-22 11:05:46 +0100

    prototype of phase slope phase unwrapping

--- toolset/processing/cs_phase_slope.py
+++ toolset/processing/cs_phase_slope.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from math import atan2
+from math import atan2, pi
 from toolset.cs_utils.cs_subevent import SubeventResults
 from toolset.cs_utils.cs_step import CSStepMode2, ToneQualityIndicatorExtensionSlot
 
@@ -14,7 +14,35 @@ def calculate_phase_slope_data(initiator: SubeventResults, reflector: SubeventRe
     for channel in common_channels:
         phase_sums[channel] = initiator_phases[channel] + reflector_phases[channel]
 
-    return phase_sums
+    return _unwrap_phases_by_channel(phase_sums)
+
+
+# TODO: test and debug
+def _unwrap_phases_by_channel(phase_by_channel: Dict[int, float]) -> Dict[int, float]:
+    if not phase_by_channel:
+        return {}
+
+    sorted_channels = sorted(phase_by_channel.keys())
+    unwrapped_phases: Dict[int, float] = {}
+
+    first_channel = sorted_channels[0]
+    previous_wrapped = phase_by_channel[first_channel]
+    unwrapped_phases[first_channel] = previous_wrapped
+
+    offset = 0.0
+    for channel in sorted_channels[1:]:
+        wrapped_phase = phase_by_channel[channel]
+        delta = wrapped_phase - previous_wrapped
+
+        if delta > pi:
+            offset -= 2.0 * pi
+        elif delta < -pi:
+            offset += 2.0 * pi
+
+        unwrapped_phases[channel] = wrapped_phase + offset
+        previous_wrapped = wrapped_phase
+
+    return unwrapped_phases
 
 
 def _extract_channel_phases(subevent: SubeventResults) -> Dict[int, float]:
