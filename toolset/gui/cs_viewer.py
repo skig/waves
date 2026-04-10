d73fef22bbd73240dd1285eadb29074b29630f49
ivan iushkov <iushkov.iv@gmail.com>
2026-04-10 21:57:53 +0200

    removed sensing tab

--- toolset/gui/cs_viewer.py
+++ toolset/gui/cs_viewer.py
@@ -8,7 +8,6 @@ from toolset.gui.steps_tab import StepsTabMixin
 from toolset.gui.plots_tab import PlotsTabMixin
 from toolset.gui.ifft_tab import IFftTabMixin
 from toolset.gui.music_tab import MusicTabMixin
-from toolset.gui.sensing_tab import SensingTabMixin
 from toolset.gui.gesture_tab import GestureTabMixin
 from matplotlib.collections import PolyCollection
 
@@ -18,7 +17,7 @@ from matplotlib.collections import PolyCollection
 _LAG_SKIP_COUNT = 3
 
 
-class CSViewer(SetupTabMixin, StepsTabMixin, PlotsTabMixin, IFftTabMixin, MusicTabMixin, SensingTabMixin, GestureTabMixin):
+class CSViewer(SetupTabMixin, StepsTabMixin, PlotsTabMixin, IFftTabMixin, MusicTabMixin, GestureTabMixin):
     """GUI for viewing Channel Sounding data"""
 
     def __init__(self, initiator_subevents: List = None, reflector_subevents: List = None, dark_mode: bool = True, ml: bool = False, on_close: Callable = None):
@@ -185,7 +184,6 @@ class CSViewer(SetupTabMixin, StepsTabMixin, PlotsTabMixin, IFftTabMixin, MusicT
         self._register_tab('ifft', 'IFFT', self._build_ifft_tab, self._update_ifft_tab)
         self._register_tab('music', 'MUSIC', self._build_music_tab, self._update_music_tab)
         if self._ml_enabled:
-            self._register_tab('sensing', 'Sensing', self._build_sensing_tab, self._update_sensing_tab)
             self._register_tab('gesture', 'Gesture', self._build_gesture_tab, self._update_gesture_tab)
 
     def _register_tab(
